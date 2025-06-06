0be22a7d21210d596ade7699752d381734062e64
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-06 09:08:00 -0600

    Se modifica AboutPage

--- src/pages/aboutUs/AboutUsPage.jsx
+++ src/pages/aboutUs/AboutUsPage.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Navbar } from '../../components/navbar'
+import { Button, ButtonGroup } from '@chakra-ui/react'
 
 export const AboutUsPage = () => {
     return (
+        <>
+        <Navbar>
+            <ButtonGroup size='md' spacing='4'>
+                <Button  bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}}>Iniciar Sesión</Button>
+                <Button bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}}>Registrar</Button>
+            </ButtonGroup>
+        </Navbar>
         <div>AboutUsPage</div>
+        </>
     )
 }
