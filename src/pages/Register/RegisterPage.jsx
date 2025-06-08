d4a8dd3121a00774843be94b71c7b8ec769b4253
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-07 22:19:30 -0600

    Se agrega navbar a registro

--- src/pages/Register/RegisterPage.jsx
+++ src/pages/Register/RegisterPage.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { Register } from '../../components/Register/Register'
-
+import Icon from '../../assets/PIZPIcon.png'
+import { Navbar } from '../../components/navbar'
+import { Box, Button, ButtonGroup, Image } from '@chakra-ui/react'
+import { useNavigate } from 'react-router'
 export const RegisterPage = () => {
     return (
         <>
+         <Navbar>
+                <Box display='flex' alignItems='center' justifyContent='flex-start' flexGrow={1} padding="0.5% 0% 0.5% 0%">
+                    <Image boxSize='3rem' src={Icon} alt='Icon PIZP'/>
+                </Box>
+                <ButtonGroup size='md' spacing='4'>
+                    <Button  bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}}>Iniciar Sesión</Button>
+                    <Button bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={()=>{navigate('/register')}}>Registrar</Button>
+                </ButtonGroup>
+            </Navbar>
             <Register/>
         </>
     )
