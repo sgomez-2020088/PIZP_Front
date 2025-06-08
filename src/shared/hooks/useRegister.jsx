cced704abb8b3007519e99aff60fb7952c48c24f
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-07 22:19:43 -0600

    Se modifica hook

--- src/shared/hooks/useRegister.jsx
+++ src/shared/hooks/useRegister.jsx
@@ -5,7 +5,6 @@ import toast from 'react-hot-toast'
 export const useRegister = () => {
     const [loading, setLoading] = useState(false)
     const [error, setError] = useState(false)
-
     const register = async(name,surname,phone,DPI,email,password )=>{
         setLoading(true)
         const user={
@@ -22,13 +21,13 @@ export const useRegister = () => {
             setError(true)
             
             if(response?.err?.response?.data?.errors){
-                
+
+
                 let arrayErrors = response?.err?.response?.data?.errors
                 for (const error of arrayErrors) {
                     return toast.error(error.msg)
                 }
             }
-            
             return toast.error(
                 response?.err?.response?.data?.msg ||
                 response?.err?.data?.msg ||
@@ -36,10 +35,22 @@ export const useRegister = () => {
             )
         }
         setError(false)
-        return toast.success('Usuario registrado correctamente')
+        console.log(response?.err?.response?.data?.msg)
+        return toast.success('Usuario registrado correctamente',{
+                style: {
+                    border: '1px solid #DE4B4B',
+                    padding: '16px',
+                    color: '#DE4B4B',
+                },
+                iconTheme: {
+                    primary: '#DE4B4B',
+                    secondary: '#F6C388',
+                },
+})
     }
     return {
         register,
-        loading
+        loading,
+
     }
 }
