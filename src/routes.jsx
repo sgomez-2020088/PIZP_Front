7c8d559d6706cbe72a4c8eae6c72deaedf435b9b
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-09 10:16:41 -0600

    Se añade nueva ruta

--- src/routes.jsx
+++ src/routes.jsx
@@ -4,11 +4,13 @@ import { NotFoudPage } from "./pages/NotFoudPage";
 import { RegisterPage } from "./pages/Register/RegisterPage";
 import { LoginPage } from "./pages/Login/LoginPage";
 import { ReportsPage } from "./pages/Reports/ReportsPage";
+import { ReportPage } from "./pages/Report/ReportPage";
 
 export const routes = [
     {path:'/', element:<AboutUsPage/>},
     {path:'/register', element:<RegisterPage/>},
     {path:'/login', element:<LoginPage/>},
     {path:'/reports', element:<ReportsPage/>},
+    {path:'/report', element:<ReportPage/>},
     {path:'*', element:<NotFoudPage/>}
 ]
