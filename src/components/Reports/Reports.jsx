c531d278ca1a511fe6d60db822b33cf598f497ee
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-10 21:21:59 -0600

    Se modifica componente Reports

--- src/components/Reports/Reports.jsx
+++ src/components/Reports/Reports.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Map } from '../Map/Map';
+import { Box } from '@chakra-ui/react';
 
 export const Reports = () => {
-        const defaultCenter = { lat: 37.7749, lng: -122.4194 };  
-        const defaultZoom = 11;
+        const defaultCenter = { lat: 14.6349, lng: -90.5069 };  
+        const defaultZoom = 14;
   return (
     <>
-        <Map center={defaultCenter} zoom={defaultZoom}/>
+    <Box 
+    display="flex" 
+    alignItems="center" 
+    justifyContent="center" 
+    flexDirection="column" 
+    width="70rem" 
+    height="34rem"  
+    style={{ margin: '5rem auto' }} 
+>
+  <Map 
+    center={defaultCenter} 
+    zoom={defaultZoom} 
+    
+  />
+</Box>
     </>
   )
 }
