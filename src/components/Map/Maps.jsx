980c71ed6318caf2fc86f2532bab864c46a066b9
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-13 23:42:08 -0600

    Se agrega nuevo mapa

---
+++ src/components/Map/Maps.jsx
@@ -0,0 +1,28 @@
+import { Text } from '@chakra-ui/react'
+import { AdvancedMarker, APIProvider, InfoWindow, Map } from '@vis.gl/react-google-maps'
+import React, { useState } from 'react'
+
+export const Maps = ({marker, handleClickChangeMarker}) => {
+
+    
+    const [open, setOpen] = useState(false)
+    const position={lat:14.60178208398095, lng:-90.54235243138898}
+
+
+    const handleClickBox = ()=>{
+        setOpen(!open)
+    }
+
+        
+  return (
+    <APIProvider apiKey='AIzaSyAhoC1Gjnmo2AYoJ8gFN5u_Lwnvumbida8'>
+        <Map defaultCenter={position} defaultZoom={11} mapId='d83b2bfcb134e57b5d056f3b' onClick={handleClickChangeMarker}>
+            <AdvancedMarker position={marker} onClick={handleClickBox}></AdvancedMarker>
+            {open&&(<InfoWindow position={marker} onCloseClick={handleClickBox}>
+                <Text>Halo</Text>
+            </InfoWindow>)}
+            
+        </Map>
+    </APIProvider>
+  )
+}
