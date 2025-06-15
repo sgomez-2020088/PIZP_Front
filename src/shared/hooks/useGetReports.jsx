edac8a7cc05ea01f11b96fe43b39c70d15e0d48e
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-15 13:46:45 -0600

    Se agrega hook useGetReports

---
+++ src/shared/hooks/useGetReports.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+import { getReportsRequest } from '../../services/api'
+import toast from 'react-hot-toast'
+
+export const useGetReports = () => {
+
+    const [reports, setReports] = useState(null)
+
+    const getReports = async()=>{
+        const response = await getReportsRequest()
+
+        if(response.error){
+            return toast.error(
+                response?.err?.response?.data?.message ||
+                'Error al crear reporte. Intente mas tarde'
+            )
+        }
+
+        setReports(response.data.reports)
+    }
+    return {
+        reports,
+        isFetching: !reports,
+        getReports
+    }
+}
