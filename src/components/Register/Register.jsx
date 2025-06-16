be82f7e012a0a7b56edcfc10f974ed16113bedfd
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-16 11:39:29 -0600

    Se modifica componente Register

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -93,45 +93,47 @@ export const Register = () => {
         <>
             <Box  display='flex' alignItems='center' justifyContent='center' flexDirection='column'  > 
                 <form onSubmit={handleSubmit}>
+                                    
                 <FormControl  display="flex" flexDirection="column" alignItems="center" justifyContent="center"width="100%"textAlign="left" maxWidth="500px">
     
-                    <Box  marginTop='5%' marginBottom='5%'>  
+                    <Box  marginTop='5%' marginBottom='4%'>  
                         <Text fontSize='3xl'  color='#DE4B4B' as='b'> REGISTRATE EN PIZP</Text>
                     </Box>
-                    <FormLabel fontSize='1.2rem' size='lg'color='#DE4B4B'width='39rem' textAlign="left">Nombres</FormLabel>
-                    <Input id='1'size='md' value={name}   width='40rem' onChange={handleChangeName} mb='3'/>
-                    <span style={{color:'#DE4B4B'}} > {validationErrors.name && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.name}</span>
+                     <Text marginBottom='1.5rem' color='#c54040'>Ya tienes cuenta?<Link to='/login'> Inicia Sesión</Link></Text>
+                   
+                    <Input  placeHolder='Nombre' id='1'size='md' value={name}   width='40rem' onChange={handleChangeName} mb='3'/>
+                    <span style={{color:'#DE4B4B', marginBottom:'1rem'}} > {validationErrors.name && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.name}</span>
                     
-                    <FormLabel  fontSize='1.2rem'  color='#DE4B4B' width='39rem' textAlign="left">Apellidos</FormLabel>  
-                    <Input size='md' value={surname} width='40rem'onChange={handleChangeSurname}mb='3'/>
-                    <span style={{color:'#DE4B4B'}} > {validationErrors.surname && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.surname}</span>
+                   
+                    <Input  placeHolder='Apellidos' size='md' value={surname} width='40rem'onChange={handleChangeSurname}mb='3'/>
+                    <span style={{color:'#DE4B4B', marginBottom:'1rem'}} > {validationErrors.surname && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.surname}</span>
                   
-                    <FormLabel  fontSize='1.2rem'  color='#DE4B4B' width='39rem' textAlign="left">Numero de telefono</FormLabel>
+                    
                     <InputGroup  width='40rem'>
                         <InputLeftAddon>+502</InputLeftAddon>
-                        <Input size='md' value={phone}  onChange={handleChangePhone}mb='3'/>
+                        <Input  placeHolder='Numero de telefono' size='md' value={phone}  onChange={handleChangePhone}mb='3'/>
                     </InputGroup>
-                    <span style={{color:'#DE4B4B'}} > {validationErrors.phone && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.phone}</span>
+                    <span style={{color:'#DE4B4B', marginBottom:'1rem'}} > {validationErrors.phone && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.phone}</span>
 
-                    <FormLabel fontSize='1.2rem'  color='#DE4B4B' width='39rem' textAlign="left">DPI</FormLabel>
-                    <Input size='md' value={DPI} width='40rem' onChange={handleChangeDPI}mb='3' />
-                    <span style={{color:'#DE4B4B'}} > {validationErrors.DPI && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.DPI}</span>
+                    
+                    <Input placeHolder='DPI' size='md' value={DPI} width='40rem' onChange={handleChangeDPI}mb='3' />
+                    <span style={{color:'#DE4B4B', marginBottom:'1rem'}} > {validationErrors.DPI && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.DPI}</span>
                    
-                    <FormLabel fontSize='1.2rem'  color='#DE4B4B' width='39rem' textAlign="left">Email</FormLabel>
-                    <Input size='md' value={email}  width='40rem' onChange={handleChangeEmail}mb='3'/>
-                     <span style={{color:'#DE4B4B'}} > {validationErrors.email && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.email}</span>
+                   
+                    <Input  placeHolder='Email' size='md' value={email}  width='40rem' onChange={handleChangeEmail}mb='3'/>
+                     <span style={{color:'#DE4B4B', marginBottom:'1rem'}} > {validationErrors.email && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.email}</span>
 
-                    <FormLabel  fontSize='1.2rem'  color='#DE4B4B' width='39rem' textAlign="left">Contraseña</FormLabel>
+                    
                     <InputGroup width='40rem'>
-                        <Input type={show?'text':'password'} size='md'  width='130%'  value={password} onChange={handleChangePassword}mb='3'/>
+                        <Input placeHolder='Contraseña' type={show?'text':'password'} size='md'  width='130%'  value={password} onChange={handleChangePassword}mb='3'/>
                     </InputGroup>
-                    <span style={{color:'#DE4B4B'}} > {validationErrors.password && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.password}</span>
+                    <span style={{color:'#DE4B4B', marginBottom:'1rem'}} > {validationErrors.password && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.password}</span>
 
-                    <FormLabel  fontSize='1.2rem'  color='#DE4B4B' width='39rem' textAlign="left">Confirmar Contraseña</FormLabel>
+                    
                     <InputGroup width='40rem'>
-                        <Input  type={show?'text':'password'} size='md'  value={confirmPassword} onChange={handleChangeConfirmPassword} mb='3'/>
+                        <Input  placeHolder='Confirmar Contraseña' type={show?'text':'password'} size='md'  value={confirmPassword} onChange={handleChangeConfirmPassword} mb='3'/>
                     </InputGroup>
-                    <span style={{color:'#DE4B4B'}} > {validationErrors.confirmPassword && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.confirmPassword}</span>
+                    <span style={{color:'#DE4B4B', marginBottom:'1rem'}} > {validationErrors.confirmPassword && <WarningTwoIcon style={{ marginRight: '6px' }} />}{validationErrors.confirmPassword}</span>
                     <br/>
                     <Checkbox  colorScheme='red' color='#DE4B4B' onChange={handleClick} mb='3'>Ver contraseña</Checkbox>
                     <Button disabled={!disabled} isLoading={loading?true:false}width='40rem' bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} borderRadius='2.5rem' _active={{bg:'#c54040'}}type='submit'>Enviar</Button>
