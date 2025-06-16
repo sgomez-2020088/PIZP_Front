d58907ff86c07d05d2f6269c9d2a2bdbd3319a23
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-16 11:02:58 -0600

    Se modifica pagina LoginPage

--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -7,17 +7,10 @@ import { Login } from '../../components/Login/Login'
 
 export const LoginPage = () => {
     const navigate = useNavigate()
-  return (
-          <>
-           <Navbar>
-                  <Box display='flex' alignItems='center' justifyContent='flex-start' flexGrow={1} padding="0.5% 0% 0.5% 0%">
-                      <Image boxSize='3rem' src={Icon} alt='Icon PIZP'/>
-                  </Box>
-                  <ButtonGroup size='md' spacing='4'>
-                      <Button  bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={()=>{navigate('/register')}}>Registrate</Button>
-                  </ButtonGroup>
-              </Navbar>
-              <Login/>
-          </>
-      )
+    return (
+        <>
+            
+                <Login/>
+        </>
+    )
 }
