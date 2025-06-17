84d890d16fe1cc06336f72a7163573ee81441175
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-16 22:17:50 -0600

    Se modifica componente Maps

--- src/components/Map/Maps.jsx
+++ src/components/Map/Maps.jsx
@@ -1,30 +1,36 @@
-import { Text } from '@chakra-ui/react'
-import { AdvancedMarker, APIProvider, InfoWindow, Map } from '@vis.gl/react-google-maps'
-import React, { useState } from 'react'
+import { Text } from '@chakra-ui/react';
+import { AdvancedMarker, APIProvider, InfoWindow, Map } from '@vis.gl/react-google-maps';
+import React, { useState } from 'react';
 
-export const Maps = ({marker, handleClickChangeMarker, description,id,children}) => {
+export const Maps = ({ marker, handleClickChangeMarker, id, children }) => {
+  const [open, setOpen] = useState(false);
+  const [address, setAddress] = useState('');  
+  const position = { lat: 14.60178208398095, lng: -90.54235243138898 };
 
-    
-    const [open, setOpen] = useState(false)
-    const position={lat:14.60178208398095, lng:-90.54235243138898}
-
-
-    const handleClickBox = ()=>{
-        setOpen(!open)
+  
+  const handleClickBox = () => {
+    setOpen(!open);
+    if (marker) {
+      getAddress(marker);  
     }
+  };
 
-        
+  
+  
+
+
+
   return (
-    <APIProvider apiKey='AIzaSyAhoC1Gjnmo2AYoJ8gFN5u_Lwnvumbida8'>
-        <Map defaultCenter={position} defaultZoom={11} mapId='d83b2bfcb134e57b5d056f3b' onClick={handleClickChangeMarker}>
-            {children}
+    <APIProvider apiKey={import.meta.env.VITE_API_KEY}>
+      <Map defaultCenter={position} defaultZoom={12} maxZoom={21} minZoom={8} mapId={import.meta.env.VITE_ID_MAP} onClick={handleClickChangeMarker}>
+        {children}
 
-            {marker &&(
-                <>
-                <AdvancedMarker key={id} position={marker} onClick={handleClickBox}></AdvancedMarker>
-                </>
-            )}
-        </Map>
+        {marker && (
+          <>
+            <AdvancedMarker key={id} position={marker} onClick={handleClickBox}></AdvancedMarker>
+          </>
+        )}
+      </Map>
     </APIProvider>
-  )
-}
+  );
+};
