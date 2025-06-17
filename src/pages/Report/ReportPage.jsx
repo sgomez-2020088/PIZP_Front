b362404477909a1fe85442989cd5f86d9cc4c292
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-16 22:21:37 -0600

    Se modifica Pagina ReportPage

--- src/pages/Report/ReportPage.jsx
+++ src/pages/Report/ReportPage.jsx
@@ -4,23 +4,36 @@ import Icon from '../../assets/PIZPIcon.png'
 import { Box, Button, ButtonGroup, Image } from '@chakra-ui/react'
 import { useNavigate } from 'react-router-dom'
 import { Report } from '../../components/Report/Report'
+import { DoNotAccess } from '../../components/DoNotAccess/DoNotAccess'
 
 export const ReportPage = () => {
 
+    const token = localStorage.getItem('token')
     const navigate = useNavigate()
+
+    const handleClickLogOut = ()=>{
+        navigate('/')
+        localStorage.removeItem('DPI')
+        localStorage.removeItem('token')
+      }
 return (
-    <>
-        <Navbar>
-            <Box display='flex' alignItems='center' justifyContent='flex-start' flexGrow={1} padding="0.5% 0% 0.5% 0%">
-                <Image boxSize='3rem' src={Icon} alt='Icon PIZP'/>
-          </Box>
-          <ButtonGroup size='md' spacing='4'>
-                <Button  bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={()=>{navigate('/reports')}}>Reportes</Button>
-                <Button  bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={()=>{navigate('/report')}}>Cerrar Sesión</Button>
-                
-        </ButtonGroup>
-        </Navbar>
-        <Report/>
+    <> 
+        {token?
+        (
+            <>
+                <Navbar>
+                    <Box display='flex' alignItems='center' justifyContent='flex-start' flexGrow={1} padding="0.5% 0% 0.5% 0%">
+                        <Image boxSize='3rem' src={Icon} alt='Icon PIZP'/>
+                </Box>
+                <ButtonGroup size='md' spacing='4'>
+                        <Button  bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={()=>{navigate('/reports')}}>Reportes</Button>
+                        <Button  bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={handleClickLogOut}>Cerrar Sesión</Button>
+                        
+                </ButtonGroup>
+                </Navbar>
+                <Report/>
+            </>
+        ):(<DoNotAccess/>)}
     </>
   )
 }
