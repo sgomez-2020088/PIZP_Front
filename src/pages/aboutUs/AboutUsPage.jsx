eb8043856fd53d0dcb3a3a42e9fc7df01b6529f7
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-18 22:48:48 -0600

    Se cambia navbar

--- src/pages/aboutUs/AboutUsPage.jsx
+++ src/pages/aboutUs/AboutUsPage.jsx
@@ -18,7 +18,7 @@ export const AboutUsPage = () => {
       }
     return (
         <>
-            <Navbar>
+            <nav style={{display:'flex',justifyContent:"flex-end",padding:"0.8% 5%", backgroundColor:'#12101C', w:'100%' ,height:'4rem' }}>
                 <Box display='flex' alignItems='center' justifyContent='flex-start' flexGrow={1} padding="0.5% 0% 0.5% 0%">
                     <Image boxSize='3rem' src='https://res.cloudinary.com/ddkdbwjnw/image/upload/v1750296800/PIZPIcon_wch4tj.png' alt='Icon PIZP'/>
                 </Box>
@@ -29,7 +29,7 @@ export const AboutUsPage = () => {
                     {token?( <Button bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={()=>{navigate('/report')}}>Reportar</Button>):('')}
                     {token?( <Button bg='#DE4B4B' color='white' _hover={{bg:'#ee5757'}} _active={{bg:'#c54040'}} onClick={handleClickLogOut}>Cerrar Sesión</Button>):('')}
                 </ButtonGroup>
-            </Navbar>
+            </nav>
         <AboutUs/>
         </>
     )
