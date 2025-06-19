62aa9c9510dc617652793cd0d578b3bd3ad9f5ea
drodas-2021170 <drodas-2021170@kinal.edu.gt>
2025-06-18 23:42:38 -0600

    Cambios varios

--- src/services/api.js
+++ src/services/api.js
@@ -3,7 +3,7 @@ import axios  from "axios";
 
 const apiClient = axios.create(
     {
-        baseURL:'pizpbackend.vercel.app',
+        baseURL:'http://localhost:3641',
         setTimeout:10000
     }
 )
